9e1e79d76e4c9573ebd05ab14535024c66133319
Elison Queiroz <elison.nunes@gmail.com>
2020-07-19 23:53:54 +0100

    layout changes
    new background
    layout changes

--- htmlcss-base-project/src/assets/js/main.js
+++ htmlcss-base-project/src/assets/js/main.js
@@ -1,4 +1,14 @@
 ( function( $ ) {
+
+
+$(".main__button").on("click",function(){
+  window.open('https://lashdesignerbrasil.com/limarecarga', '_blank');
+})
+
+
+
+
+
   function init() {
     console.log( "init" );
   }
